894465ee4183c5dc2d8522d47f8c9a447d0a9861
ZombiMaks <kolesnikmaksim8250@gmail.com>
2018-07-21 16:06:49 +0300

    Обработка запросов

--- public/index.php
+++ public/index.php
@@ -2,16 +2,43 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$configuration = [
-    'settings' => [
-        'displayErrorDetails' => true,
-    ],
+$app = new \Slim\App();
+
+$companies = [
+    [ 'name' => 'Adams-Reichel','phone' => '1-986-987-9109 x56053' ],
+    [ 'name' => 'Dibbert-Morissette','phone' => '439.584.3132 x735' ],
+    [ 'name' => 'Ledner and Sons','phone' => '979-539-4173 x048' ],
+    [ 'name' => 'Kiehn-Mann','phone' => '972-379-1995 x61054' ],
+    [ 'name' => 'Bosco, Pouros and Larson','phone' => '887-919-2730 x49977' ],
+    [ 'name' => 'Ledner and Sons','phone' => '979-539-4173 x048' ],
+    [ 'name' => 'Adams-Reichel','phone' => '1-986-987-9109 x56053' ],
+    [ 'name' => 'Dibbert-Morissette','phone' => '439.584.3132 x735' ],
+    [ 'name' => 'Ledner and Sons','phone' => '979-539-4173 x048' ],
+    [ 'name' => 'Kiehn-Mann','phone' => '972-379-1995 x61054' ],
+    [ 'name' => 'Bosco, Pouros and Larson','phone' => '887-919-2730 x49977' ],
+    [ 'name' => 'Ledner and Sons','phone' => '979-539-4173 x048' ],
+    [ 'name' => 'Adams-Reichel','phone' => '1-986-987-9109 x56053' ],
+    [ 'name' => 'Dibbert-Morissette','phone' => '439.584.3132 x735' ],
+    [ 'name' => 'Ledner and Sons','phone' => '979-539-4173 x048' ],
+    [ 'name' => 'Kiehn-Mann','phone' => '972-379-1995 x61054' ],
+    [ 'name' => 'Bosco, Pouros and Larson','phone' => '887-919-2730 x49977' ],
+    [ 'name' => 'Ledner and Sons','phone' => '979-539-4173 x048' ],
+    [ 'name' => 'Adams-Reichel','phone' => '1-986-987-9109 x56053' ],
+    [ 'name' => 'Dibbert-Morissette','phone' => '439.584.3132 x735' ],
+    [ 'name' => 'Ledner and Sons','phone' => '979-539-4173 x048' ],
+    [ 'name' => 'Kiehn-Mann','phone' => '972-379-1995 x61054' ],
+    [ 'name' => 'Bosco, Pouros and Larson','phone' => '887-919-2730 x49977' ],
+    [ 'name' => 'Ledner and Sons','phone' => '979-539-4173 x048' ]
 ];
 
-$app = new \Slim\App($configuration);
+$app->get('/companies', function ($request, $response) use ($companies) {
+    $page = $request->getQueryParam('page', 1);
+    $per = $request->getQueryParam('per', 5);
+    $offset = ($page - 1) * $per;
 
-$app->get('/', function ($request, $response) {
-    $response->write('Welcome to Slim!');
+    $sliceOfCompanies = array_slice($companies, $offset, $per);
+    $response->write(json_encode($sliceOfCompanies));
     return $response;
 });
+
 $app->run();
