e5f1b397ff07ecd4ae29b047b6fb2b460c1e08e4
ZombiMaks <kolesnikmaksim8250@gmail.com>
2018-07-23 21:05:21 +0300

    Search

--- public/index.php
+++ public/index.php
@@ -2,6 +2,7 @@
 namespace App;
 
 require __DIR__ . '/../vendor/autoload.php';
+use function Stringy\create as s;
 
 $users = Generator::generate(100);
 
@@ -15,6 +16,20 @@ $app = new \Slim\App($configuration);
 // Подключение шаблонизатора к слиму. Указываем папку с шаблонами
 $container = $app->getContainer();
 $container['renderer'] = new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+
+// Поиск пользователей
+$app->get('/search', function ($request, $response) use ($users){
+    $term = $request->getQueryParam('term', '');
+    $sortedUsers = collect($users)->sortBy('firstName');
+    $userSearch = collect($sortedUsers)->filter(function ($user) use ($term) {
+        return s($user['firstName'])->startsWith($term, false);
+    });
+    $params = [
+        'userSearch' => $userSearch,
+        'term' => $term
+        ];
+    return $this->renderer->render($response, 'users/search.phtml', $params);
+});
 
 // Выводит список пользователей
 $app->get('/users', function ($request, $response) use ($users) {
